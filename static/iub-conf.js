7d8d60945a357ab1a4f5717e1e4f45af06f98be8
Alberto Masia <alberto.masia@gmail.com>
2019-09-18 18:12:23 +0200

    Add option to cookie banner conf

--- static/iub-conf.js
+++ static/iub-conf.js
@@ -1,2 +1,18 @@
 var _iub = _iub || [];
-_iub.csConfiguration = { "lang": "en", "siteId": 1676524, "cookiePolicyId": 78756685, "banner": { "acceptButtonDisplay": true, "customizeButtonDisplay": false, "position": "bottom", "acceptButtonColor": "#00d1b2", "acceptButtonCaptionColor": "#ffffff", "customizeButtonColor": "#cccccc", "customizeButtonCaptionColor": "#333333", "textColor": "#333333", "backgroundColor": "#EDEDED" } };
+_iub.csConfiguration = {
+  "lang": "en",
+  "siteId": 1676524,
+  "consentOnContinuedBrowsing":false,
+  "cookiePolicyId": 78756685,
+  "banner": {
+    "acceptButtonDisplay": true,
+    "customizeButtonDisplay": false,
+    "position": "bottom",
+    "acceptButtonColor": "#00d1b2",
+    "acceptButtonCaptionColor": "#ffffff",
+    "customizeButtonColor": "#cccccc",
+    "customizeButtonCaptionColor": "#333333",
+    "textColor": "#333333",
+    "backgroundColor": "#EDEDED"
+  }
+};
